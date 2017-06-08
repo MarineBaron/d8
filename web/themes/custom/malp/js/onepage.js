888dc9c49a6121a7986681011e4cf5da0f72c780
Marine Baron <marine17@marinealaplage.dev>
2017-06-08 13:32:50 +0200

    FIX  : js onepage

--- web/themes/custom/malp/js/onepage.js
+++ web/themes/custom/malp/js/onepage.js
@@ -9,50 +9,52 @@
     Drupal.behaviors.malp_onepage = {
       attach : function (context, settings) {
 
-        // Scroll.
-        if (drupalSettings.malp.onepage) {
-          $('body').scrollspy({target: drupalSettings.malp.navbar, offset: 50});
-          $(drupalSettings.malp.navbar).on('click', 'a', function (event) {
-            var $hash = this.hash;
-            if ($hash.length && $($hash).length) {
-              event.preventDefault();
-              $('html, body').animate({
-                scrollTop: $($hash).offset().top
-              }, 800, function () {
-                window.location.hash = $hash;
-              });
-            }
-          });
-
-          // Fullscreen carousel.
-          var $item = $('.carousel-inner .carousel-item');
-          var $wHeight = $(window).height();
-
-          $item.height($wHeight);
-          $item.addClass('full-screen');
-
-          $item.each(function () {
-            var $img = $(this).find('img').eq(0);
-            var $src = $img.attr('src');
-            $(this).css({
-              'background-image' : 'url(' + $src + ')',
+        $('body').once('malp_onepage').each(function () {
+          // Scroll.
+          if (drupalSettings.malp.onepage) {
+            $('body').scrollspy({target: drupalSettings.malp.navbar, offset: 50});
+            $(drupalSettings.malp.navbar).on('click', 'a', function (event) {
+              var $hash = this.hash;
+              if ($hash.length && $($hash).length) {
+                event.preventDefault();
+                $('html, body').animate({
+                  scrollTop: $($hash).offset().top
+                }, 800, function () {
+                  window.location.hash = $hash;
+                });
+              }
             });
-            $img.remove();
-          });
 
-          $(window).on('resize', function () {
-            $wHeight = $(window).height();
+            // Fullscreen carousel.
+            var $item = $('.carousel-inner .carousel-item');
+            var $wHeight = $(window).height();
+
             $item.height($wHeight);
-          });
+            $item.addClass('fullscreen');
 
-          // Button next.
-          $('.btn-next-block').on('click', function () {
-            var $elt = $('.region-content section.block').eq(0);
-            $('html, body').animate({
-              scrollTop: $elt.offset().top
-            }, 800);
-          });
-        }
+            $item.each(function () {
+              var $img = $(this).find('img').eq(0);
+              var $src = $img.attr('src');
+              $(this).css({
+                'background-image' : 'url(' + $src + ')',
+              });
+              $img.remove();
+            });
+
+            $(window).on('resize', function () {
+              $wHeight = $(window).height();
+              $item.height($wHeight);
+            });
+
+            // Button next.
+            $('.btn-next-block').on('click', function () {
+              var $elt = $('.region-content section.block').eq(0);
+              $('html, body').animate({
+                scrollTop: $elt.offset().top
+              }, 800);
+            });
+          }
+        });
       }
     };
 })(jQuery, Drupal, drupalSettings);
