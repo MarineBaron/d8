ac41be0f9cfe8000dadfb46c9b547e1bffd6f068
Marine Baron <marine17@marinealaplage.dev>
2017-06-23 12:56:10 +0200

    FIX : onepagejs

--- web/themes/custom/malp/js/onepage.js
+++ web/themes/custom/malp/js/onepage.js
@@ -16,7 +16,7 @@
             $('body').scrollspy({target: drupalSettings.malp.navbar, offset: 50});
             $(drupalSettings.malp.navbar).on('click', 'a', function (event) {
               var $hash = this.hash;
-              if ($hash.length && $($hash).length()) {
+              if ($hash.length && $($hash).length) {
                 event.preventDefault();
                 $('html, body').animate({
                   scrollTop: $($hash).offset().top
